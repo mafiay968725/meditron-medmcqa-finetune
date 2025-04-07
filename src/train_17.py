03614da76eff144ad3dc7387fb4482e2ad18bfda
mafiay968725 <2591045161@qq.com>
2025-04-08 00:33:51 +0800

    update

--- src/train_17.py
+++ src/train_17.py
@@ -202,7 +202,6 @@ def log_final_accuracy_to_csv(lora_rank, dropout, lr, accuracy, log_path, is_fin
 
 # ✅ Top 5 hyperparameter sets based on previous results
 top_configs = [
-    {"lora_rank": 16, "dropout": 0.24, "lr": 0.0001},
     {"lora_rank": 16, "dropout": 0.24, "lr": 0.00013}
 ]
 
